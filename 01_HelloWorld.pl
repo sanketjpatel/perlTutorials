11071b73eda9947c9ff18e039f964b0ef793480e
sanketjpatel <sanketpatel.301090@gmail.com>
2014-11-29 16:27:42 -0500

    Add comment about syntax for naming identifiers

--- 01_HelloWorld.pl
+++ 01_HelloWorld.pl
@@ -6,6 +6,9 @@ print 'Hello World!\n';
 print "\n";
 
 #Backslash can be used to escape characters
+#A Perl identifier name starts with either $, @ or % followed by zero or more letters, underscores, and digits
+#Punctuation characters such as @, $, and % are not allowed within identifiers
+#Perl is a case sensitive programming language
 $result = "\nThis is \"number\"";
 print "$result\n";
 print "\$result\n"; 
