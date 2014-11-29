a9a0ae3576afb1db84ae56648d1d272ed5734162
sanketjpatel <sanketpatel.301090@gmail.com>
2014-11-28 21:29:58 -0500

    Create "Here" Documents file, add backslash example to HelloWorld

--- 01_HelloWorld.pl
+++ 01_HelloWorld.pl
@@ -1,4 +1,11 @@
 print "Hello World!\n";
 
 #Single quotes will not interpolate variables and special characters
-print 'Hello World!\n'; 
+print 'Hello World!\n';
+
+print "\n";
+
+#Backslash can be used to escape characters
+$result = "\nThis is \"number\"";
+print "$result\n";
+print "\$result\n"; 
