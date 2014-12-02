8afabbe73e23a4f0c5874b8a0ca24ca0f039c449
sanketjpatel <sanketpatel.301090@gmail.com>
2014-12-01 23:55:40 -0500

    Add more examples

--- 04_Scalars.pl
+++ 04_Scalars.pl
@@ -1,10 +1,10 @@
-$age = 25;             # An integer assignment
-$name = "John Paul";   # A string 
-$float = 1445.50;     # A floating point
+$age = 24;                # An integer assignment
+$name = "Sanket Patel";   # A string 
+$float = 1445.50;         # A floating point
 
 print "Age = $age\n";
 print "Name = $name\n";
-print "Float = $float\n";
+print "Float = $float\n\n";
 
 # Numeric Scalars
 $integer = 200;
@@ -23,32 +23,34 @@ print "negative = $negative\n";
 print "floating = $floating\n";
 print "bigfloat = $bigfloat\n";
 print "octal = $octal\n";
-print "hexa = $hexa\n";
+print "hexa = $hexa\n\n";
 
 # Scalar Operations
-$str = "hello" . "world";       # Concatenates strings.
-$num = 5 + 10;                  # adds two numbers.
-$mul = 4 * 5;                   # multiplies two numbers.
+$str = "hello" . "world";       # Concatenation
+$num = 5 + 10;                  # addition
+$mul = 4 * 5;                   # multiplication
 $mix = $str . $num;             # concatenates string and number.
 
 print "str = $str\n";
 print "num = $num\n";
 print "mul = $mul\n";
-print "mix = $mix\n";
+print "mix = $mix\n\n";
 
-# V-strings
+# V-strings - Use ASCII numbers
 $smile  = v9786;
+$music = v9835;
 $foo    = v102.111.111;
 $martin = v77.97.114.116.105.110; 
 
 print "smile = $smile\n";
+print "music = $music\n";
 print "foo = $foo\n";
-print "martin = $martin\n";
+print "martin = $martin\n\n";
 
 # These are special literals
 print "File name ". __FILE__ . "\n";
 print "Line Number " . __LINE__ ."\n";
-print "Package " . __PACKAGE__ ."\n";
+print "Package " . __PACKAGE__ ."\n\n";
 
 # they can not be interpolated
 print "__FILE__ __LINE__ __PACKAGE__\n";
