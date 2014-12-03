62a5693bb4d5f1af80699ba42e0bd62e28fdd705
sanketjpatel <sanketpatel.301090@gmail.com>
2014-12-03 00:01:09 -0500

    Create Hashes

---
+++ 06_Hashes.pl
@@ -0,0 +1,12 @@
+%data = ('John Paul', 45, 'Lisa', 30, 'Kumar', 40);
+
+print "\$data{'John Paul'} = $data{'John Paul'}\n";
+print "\$data{'Lisa'} = $data{'Lisa'}\n";
+print "\$data{'Kumar'} = $data{'Kumar'}\n";
+
+%ages = ('Sanket' => 23, 'Tejas' => 22, 'Pankaj' => 24);
+
+%anotherHash = (-keyone => 25, -keytwo => 30, -keythree => 43);
+
+print %ages."\n";
+print %anotherHash."\n";
